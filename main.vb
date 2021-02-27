a3a42aa2fbf4b4aff8769b709449f421804e0639
AlcaTechStudio <dimeletr@gmail.com>
2021-02-27 15:21:05 -0300

    Todo Librarys

--- main.vb
+++ main.vb
@@ -1,61 +1,18 @@
 imports "\system\genesis_header.asm" ' Header de uma ROM de mega Drive Padrão (deve ficar sempre no topo)
 
-#const gravidade = 100
 
-#if gravidade > 30 Then 
- teste = 1
-#Else
- teste = 10
-#End If
+std_init()   'Inicializa o VDP
+print_init() 'Carrega a fonte para a VRAM
 
-teste = _char("M")
-
-std_init()
-print_init()
-
-'[...] O que for colocado aqui sera executado apenas uma vez
-
-y=200
-
-set_sprite_size(0,0,0)
-set_sprite_gfx(0,1,0)
-
-'dim teste as signed fixed = -2.01
-
-'print_signed_fixed(teste) : println("")
-'print_signed_fixed(_signed(teste \ 2.0)) : println("")
-
-print_hex(&hFF632)
-enable_global_int()
+print("  Por enquanto ta tudo tranquilo  o.O  ")
 
 Do 'main
-'teste += teste
- 
- x+=teste
- 'x+= ret_fix()
-' _asm_block #__
-' move.w _global_teste,D0
-' asr.w  #7,D0
-' add.w D0,_global_x
-'__# _asm_block_end  
 
 
-set_sprite_position(0,x,y)
-update_sprite_table()
-
-flag_vb=1
-while(flag_vb) : wend
 Loop ' Laço infinito
 
-sub isr_vblank()
-flag_vb=0
-end sub
-
-function ret_fix() as signed fixed
-  
-  return teste
-
-end function
 
 Imports "\system\genesis_std.nbs" ' Biblioteca contendo funções standard do Mega Drive
-imports "\system\print_lib.nbs"
+Imports "\system\print_lib.nbs"   ' Inclui a funcao Print
+Imports "\system\DMA_Queue.nbs"   ' Fila de DMA
+Imports "\system\trig.nbs"        ' Trigonometria para matematica de ponto Fixo
--- make.bat
+++ make.bat
@@ -1,5 +1,7 @@
 @echo off
 "%cd%\compiler\NEXTBASIC.exe" "%cd%\main.vb" -rFF0000 -sFFFFFF -a -v
 echo.
-rem echo iniciando emulador...
-"%cd%\emu\gens.exe" "%cd%\main.bin"
+echo iniciando emulador...
+cd "%cd%\emu\" 
+gens.exe "..\main.bin"
+cd ..
